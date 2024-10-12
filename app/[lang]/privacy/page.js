25e5d3f48be7d391dbb1514d7e647998720361f3
muhang <zhoudongbin.zdb@alibaba-inc.com>
2024-10-12 17:25:41 +0800

    update

--- app/[lang]/privacy/page.js
+++ app/[lang]/privacy/page.js
@@ -12,20 +12,20 @@ export default function Privacy() {
                     Our goal is to provide quality service to all customers. Please read these terms of service carefully before you access the site or the service.
                 </p>
                 <p>
-                    These Terms of Service (“the Terms”) govern your use of XnetVPN and the associated website domains (the “Site”) and related service (the “Services”), which are owned and operated by the XnetVPN Team (“XnetVPN,” “our,” “we,” or “us”). The Terms constitute a legally binding agreement (the “Agreement”) between you and the XnetVPN Team.
+                    These Terms of Service (&quot;the Terms&quot;) govern your use of XnetVPN and the associated website domains (the &quot;Site&quot;) and related service (the &quot;Services&quot;), which are owned and operated by the XnetVPN Team (&quot;XnetVPN,&quot; &quot;our,&quot; &quot;we,&quot; or &quot;us&quot;). The Terms constitute a legally binding agreement (the &quot;Agreement&quot;) between you and the XnetVPN Team.
                 </p>
             </section>
             
             <section>
                 <h2>XnetVPN Service</h2>
                 <p>
-                    You understand that we are providing you with a virtual private network (or "VPN") service, which includes, but is not limited to, the use of servers, transport, routers, IP addresses, and other equipment and protocols to transmit information over our network (the "System"). You agree to abide by the Terms with respect to your use of the Service.
+                    You understand that we are providing you with a virtual private network (or &quot;VPN&quot;) service, which includes, but is not limited to, the use of servers, transport, routers, IP addresses, and other equipment and protocols to transmit information over our network (the &quot;System&quot;). You agree to abide by the Terms with respect to your use of the Service.
                 </p>
             </section>
             
             <section>
                 <h2>Usage Policy</h2>
-                <p>BY USING THE XNETVPN SERVICE (the "Service") YOU AGREE:</p>
+                <p>BY USING THE XNETVPN SERVICE (the &quot;Service&quot;) YOU AGREE:</p>
                 <ul>
                     <li>NOT to use the system for sending spam, port scanning, or unsolicited email.</li>
                     <li>NOT to launch any pop-ups from our service.</li>
@@ -46,7 +46,7 @@ export default function Privacy() {
             <section>
                 <h2>Disclaimers</h2>
                 <p>
-                    We strive to prevent interruptions, but services are provided on an “as is” basis. Coverage, speed, and quality may vary.
+                    We strive to prevent interruptions, but services are provided on an &quot;as is&quot; basis. Coverage, speed, and quality may vary.
                 </p>
                 <ul>
                     <li>We do not warrant uninterrupted or error-free service. You access the Site and Service at your own risk.</li>
@@ -61,7 +61,7 @@ export default function Privacy() {
                     XnetVPN may update the Terms without notice. Changes will be effective upon posting. Material changes will be communicated via the App or Website.
                 </p>
             </section>
-
+            
             <section>
                 <h2>Last but NOT Least</h2>
                 <p>
